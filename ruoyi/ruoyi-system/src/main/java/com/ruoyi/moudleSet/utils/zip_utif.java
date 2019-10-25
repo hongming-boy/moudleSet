a396668e20f408cb1f3705734ce003c83509d190
zls-boy <zeng1995>
2019-10-25 17:00:06 +0800

    lingshuai→提交压缩包直接读取

---
+++ ruoyi/ruoyi-system/src/main/java/com/ruoyi/moudleSet/utils/zip_utif.java
@@ -0,0 +1,42 @@
+package com.ruoyi.moudleSet.utils;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class zip_utif {
+    public static void main(String[] args) throws IOException {
+
+        //获取文件输入流
+        FileInputStream input = new FileInputStream("C:\\Users\\Administrator\\Desktop\\test\\test.zip");
+
+        //获取ZIP输入流(一定要指定字符集Charset.forName("GBK")否则会报java.lang.IllegalArgumentException: MALFORMED)
+        ZipInputStream zipInputStream = new ZipInputStream(new BufferedInputStream(input), Charset.forName("GBK"));
+
+        //定义ZipEntry置为null,避免由于重复调用zipInputStream.getNextEntry造成的不必要的问题
+        ZipEntry ze = null;
+
+        //循环遍历
+        while ((ze = zipInputStream.getNextEntry()) != null) {
+
+            System.out.println("文件名：" + ze.getName() + " 文件大小：" + ze.getSize() + " bytes");
+            System.out.println("文件内容：");
+
+            //读取
+            BufferedReader br = new BufferedReader(new InputStreamReader(zipInputStream,Charset.forName("GBK")));
+
+            String line;
+
+            //内容不为空，输出
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+
+        //一定记得关闭流
+        zipInputStream.closeEntry();
+        input.close();
+    }
+
+}
